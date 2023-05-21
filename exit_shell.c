a212d77bc8e646e0011e023e0bd27733c2d7e531
ASANTE <asantemma7@gmail.com>
2023-05-21 21:32:41 +0000

    exits the shell

---
+++ exit_shell.c
@@ -0,0 +1,17 @@
+nclude "shell.h"
+
+/**
+ * exit_sh - exits the shell
+ * @args: array of arguments
+ * Return: void
+ */
+void exit_sh(char **args)
+{
+int status = 0;
+
+if (args[1] != NULL)
+{
+status = _atoi(args[1]);
+}
+exit(status);
+}
